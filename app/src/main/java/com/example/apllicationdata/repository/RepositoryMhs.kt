9e177fb1d56a41923417e6f827108afdb34bbb59
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-16 12:01:09 +0700

    Memanggil fungsi  getAllMahasiswa dan getMahasiswa dari mahasiswaDAO

--- app/src/main/java/com/example/apllicationdata/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/apllicationdata/repository/RepositoryMhs.kt
@@ -1,7 +1,12 @@
 package com.example.apllicationdata.repository
 
 import com.example.apllicationdata.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs () : Flow<List<Mahasiswa>>
+
+    fun getMhs (nim : String) : Flow<Mahasiswa>
 }
