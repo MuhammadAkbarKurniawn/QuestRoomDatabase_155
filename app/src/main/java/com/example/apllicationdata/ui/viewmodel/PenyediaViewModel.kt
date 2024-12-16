d6fbcca802a4f3e1c434c60342635fd7ce3231ea
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-16 22:45:23 +0700

    Melengkapi code PenyediaViewModel dengan mendaftarkan viewmodel yang suda dibuat

--- app/src/main/java/com/example/apllicationdata/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/apllicationdata/ui/viewmodel/PenyediaViewModel.kt
@@ -2,15 +2,36 @@ package com.example.apllicationdata.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.room.Update
 import com.example.apllicationdata.KrsApp
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
